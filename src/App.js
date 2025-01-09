0c1e614b4be8a8e5309c9bd5927e2fb8c494d0dd
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-08 21:47:33 -0500

    updated source files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Home from "./pages/home/Home";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Feed from "./components/feed/Feed";
+import Rightbar from "./components/rightbar/Rightbar";
+import Profile from "./components/profile/Profile";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    //   <Home />
+    // </div>
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
