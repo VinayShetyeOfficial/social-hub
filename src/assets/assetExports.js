74605828e2702ec0d11cd38f5ed1aed4fabd4808
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-08 21:46:51 -0500

    added assets files

---
+++ src/assets/assetExports.js
@@ -0,0 +1,35 @@
+// Importing person images
+export const personImages = {
+  person1: require("./person/1.jpeg"),
+  person2: require("./person/2.jpeg"),
+  person3: require("./person/3.jpeg"),
+  person4: require("./person/4.jpeg"),
+  person5: require("./person/5.jpeg"),
+  person6: require("./person/6.jpeg"),
+  person7: require("./person/7.jpeg"),
+  person8: require("./person/8.jpeg"),
+  person9: require("./person/9.jpeg"),
+  person10: require("./person/10.jpeg"),
+};
+
+// Importing post images
+export const postImages = {
+  post1: require("./post/1.jpeg"),
+  post2: require("./post/2.jpeg"),
+  post3: require("./post/3.jpeg"),
+  post4: require("./post/4.jpeg"),
+  post5: require("./post/5.jpeg"),
+  post6: require("./post/6.jpeg"),
+  post7: require("./post/7.jpeg"),
+  post8: require("./post/8.jpeg"),
+  post9: require("./post/9.jpeg"),
+  post10: require("./post/10.jpeg"),
+};
+
+// Importing miscellaneous assets
+export const miscAssets = {
+  ad: require("./ad.png"),
+  gift: require("./gift.png"),
+  heart: require("./heart.png"),
+  like: require("./like.png"),
+};
