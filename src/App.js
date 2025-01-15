d37054f8b093a687d404c89f5aae8ce7baba52f7
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-14 23:02:11 -0500

    updated with Navigate router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AuthContext } from "./context/AuthContext"; // Ensure to import AuthContext
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        {/* If user exists, navigate to Home, otherwise go to Register */}
+        <Route path="/" element={user ? <Home /> : <Register />} />
+
+        {/* Navigate to Home if user exists, otherwise display Login */}
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+
+        {/* Navigate to Home if user exists, otherwise display Register */}
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+
+        {/* Profile page */}
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
