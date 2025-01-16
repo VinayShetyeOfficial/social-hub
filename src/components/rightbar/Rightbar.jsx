97815a63b386f077d9c0543c4428413bd0857686
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-15 22:42:05 -0500

    updated rightbar to display values dynamically

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,7 @@ import { Users } from "../../data/mockData.js";
 import Online from "../online/Online.jsx";
 import "./rightbar.css";
 
-const Rightbar = ({ profile }) => {
+const Rightbar = ({ user }) => {
   const HomeRightbar = () => {
     return (
       <>
@@ -31,15 +31,28 @@ const Rightbar = ({ profile }) => {
         <div className="rightbarInfo">
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">New York</span>
+            <span className="rightbarInfoValue">{user.city}</span>
           </div>
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">Madrid</span>
+            <span className="rightbarInfoValue">{user.from}</span>
           </div>
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">Single</span>
+            <span className="rightbarInfoValue">
+              {(() => {
+                switch (user.relationship) {
+                  case 1:
+                    return "Single";
+                  case 2:
+                    return "Married";
+                  case 3:
+                    return "Complicated";
+                  default:
+                    return "-";
+                }
+              })()}
+            </span>
           </div>
         </div>
         <h4 className="rightbarTitle">User friends</h4>
@@ -99,7 +112,7 @@ const Rightbar = ({ profile }) => {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+        {user ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   );
