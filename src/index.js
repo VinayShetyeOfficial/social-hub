c61dab45309bcc6fa11c9d3dbd8fbd491a5b996d
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-15 22:44:50 -0500

    updated index code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import axios from "axios";
+import { AuthContextProvider } from "./context/AuthContext";
+import "./index.css";
+
+// Set the base URL for all Axios requests
+axios.defaults.baseURL = "http://localhost:8800/api";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
