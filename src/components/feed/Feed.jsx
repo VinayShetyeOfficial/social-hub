1de7c4af90853c9118a9745c636b4cb10e846dad
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-15 22:41:12 -0500

    added fetch posts logic

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import Share from "../share/Share";
 import Post from "../post/Post";
-import { Posts } from "../../data/mockData";
+// import { Posts } from "../../data/mockData";
+import axios from "axios";
 import "./feed.css";
 
-const Feed = () => {
+const Feed = ({ username }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = username
+          ? await axios.get("/posts/profile/" + username)
+          : await axios.get("/posts/timeline/6781a977d915bc001cfba98e");
+        setPosts(res.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, [username]);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((post) => (
-          <Post key={post.id} post={post} />
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
         ))}
       </div>
     </div>
