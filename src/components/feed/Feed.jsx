3adad1eedf2ae558067dbf369b36179c6977cca3
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-19 22:35:03 -0500

    updated

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,34 +1,48 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Share from "../share/Share";
 import Post from "../post/Post";
-// import { Posts } from "../../data/mockData";
 import axios from "axios";
 import "./feed.css";
+import { AuthContext } from "../../context/AuthContext";
 
 const Feed = ({ username }) => {
   const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
 
+  // Fetch posts when component mounts or dependencies change
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = username
-          ? await axios.get("/posts/profile/" + username)
-          : await axios.get("/posts/timeline/6781a977d915bc001cfba98e");
-        setPosts(res.data);
+          ? await axios.get(`/posts/profile/${username}`)
+          : await axios.get(`/posts/timeline/${user._id}`);
+        setPosts(
+          res.data.sort((p1, p2) => {
+            return new Date(p2.createdAt) - new Date(p1.createdAt);
+          })
+        );
       } catch (error) {
         console.error("Error fetching posts:", error);
       }
     };
 
     fetchPosts();
-  }, [username]);
+  }, [username, user._id]);
+
+  // Function to handle adding a new post to the state
+  const handleNewPost = (newPost) => {
+    setPosts((prevPosts) => [newPost, ...prevPosts]); // Add new post to the top of the feed
+  };
 
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Share />
-        {posts.map((post) => (
-          <Post key={post._id} post={post} />
+        {/* Pass handleNewPost to Share */}
+        {(!username || username === user.username) && (
+          <Share onNewPost={handleNewPost} />
+        )}
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
