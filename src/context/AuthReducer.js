c0a3f40b64679d4f83b088739e76bca5189f16ac
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-20 23:02:11 -0500

    updated AuthReducer

--- src/context/AuthReducer.js
+++ src/context/AuthReducer.js
@@ -6,21 +6,36 @@ const AuthReducer = (state, action) => {
         isFetching: true,
         error: false,
       };
-
     case "LOGIN_SUCCESS":
       return {
         user: action.payload,
         isFetching: false,
         error: false,
       };
-
     case "LOGIN_FAILURE":
       return {
         user: null,
         isFetching: false,
-        error: action.payload,
+        error: true,
       };
-
+    case "FOLLOW":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          followings: [...state.user.followings, action.payload],
+        },
+      };
+    case "UNFOLLOW":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          followings: state.user.followings.filter(
+            (following) => following !== action.payload
+          ),
+        },
+      };
     default:
       return state;
   }
